e29fc57f5f01cd76ff8da13b2ab5f90906deb6a3
jonathan gea <78746871+JonathanGea@users.noreply.github.com>
2024-12-17 09:41:43 +0700

    refactor: order service

--- src/app/components/costumer/menu/menu.component.ts
+++ src/app/components/costumer/menu/menu.component.ts
@@ -4,7 +4,7 @@ import { Product } from 'src/app/models/product.model';
 import { Order } from 'src/app/models/order.model';
 import { CategoryService } from 'src/app/service/category.service';
 import { ProductService } from 'src/app/service/product.service';
-import { OrderService } from 'src/app/service/order.service';
+import { OrderService } from 'src/app/service/order.costumer.service';
 
 @Component({
   selector: 'app-menu',
@@ -12,9 +12,8 @@ import { OrderService } from 'src/app/service/order.service';
   styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent {
-  categories?: Category[];
+  categories?:   Category[];
   products: Product[] = [];
-  order: Order = { userId: '19282bbd-b7a6-48f8-9ef4-b62aace832aa', productOrders: [] };
 
   cartItemCount = 0;
   totalPrice = 0;
@@ -24,16 +23,12 @@ export class MenuComponent {
   constructor(
     private categoryService: CategoryService,
     private productService: ProductService,
-    private orderService: OrderService
+    public orderService: OrderService
   ) {}
 
   ngOnInit() {
-    console.log('ngOnInit :>> ');
     this.getCategory();
     this.getProducts();
-    this.order = this.orderService.getOrder() || this.order;
-    this.updateData();
-    
   }
 
   getCategory() {
@@ -67,57 +62,10 @@ export class MenuComponent {
     );
   }
 
-  placeOrder() {
-    this.orderService.addOrder(this.order);
-    console.log('Order placed:', this.order); // Untuk debugging
-  }
 
-  decreaseQuantity(product: Product) {
-    // Check if the product is already in the cart
-    const existingProduct = this.order?.productOrders.find(
-      (item) => item.id === product.id
-    );
 
-    if (existingProduct) {
-      existingProduct.quantity -= 1;
-      if (existingProduct.quantity < 1) {
-        this.order.productOrders = this.order.productOrders.filter(
-          (item) => item.id !== product.id
-        );
-      }
-    }
 
-    console.log('this.order :>> ', this.order);
-    this.updateData()
-  }
 
-  increaseQuantity(product: Product) {
-    // Check if the product is already in the cart
-    const existingProduct = this.order?.productOrders.find(
-      (item) => item.id === product.id
-    );
-
-    if (existingProduct) {
-      existingProduct.quantity += 1;
-    } else {
-      this.order?.productOrders.push({
-        id: product.id,
-        quantity: 1,
-        name: product.name,
-        price: product.price,
-        urlImage: product.urlImage
-      });
-    }
-    console.log('this.order :>> ', this.order);
-    this.updateData()
-  }
-
-  getProductQuantity(productId: string): number {
-    const productOrder = this.order.productOrders.find(
-      (item) => item.id === productId
-    );
-    return productOrder ? productOrder.quantity : 0;
-  }
   // end - service
 
   formatPriceToRupiah(price: number): string {
@@ -171,15 +119,9 @@ export class MenuComponent {
     if (!foundActiveCategory) {
       this.activeCategoryId = null;
     }
-    console.log('this.activeCategoryId :>> ', this.activeCategoryId);
+    // console.log('this.activeCategoryId :>> ', this.activeCategoryId);
   }
 
 
 
-  updateData(){
-    this.cartItemCount = this.order.productOrders.length;
-    this.order.totalPrice = this.order.productOrders.reduce((total, item) => {
-      return total + (item.price * item.quantity);
-    }, 0);
-  }
 }
---
+++ src/app/service/order.costumer.service.ts
@@ -0,0 +1,152 @@
+import { Injectable } from '@angular/core';
+import { Order } from '../models/order.model';
+import { catchError, Observable, of, tap } from 'rxjs';
+import { environment } from '../environment/environment';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../models/product.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OrderService {
+  private apiUrl = `${environment.API_URL}/order`;
+  private storageKey = 'costumerOrder'; // Key for localStorage
+
+  order: Order = {
+    userId: '19282bbd-b7a6-48f8-9ef4-b62aace832aa',
+    productOrders: [],
+  };
+
+  constructor(private http: HttpClient) {}
+
+  getOrder(): Order | undefined {
+    const storedOrder = localStorage.getItem(this.storageKey); // Retrieve the order from local storage
+    if (storedOrder) {
+      this.order = JSON.parse(storedOrder); // Parse the JSON string back to an Order object
+      return this.order; // Return the current order
+    }
+    return undefined; // Return undefined if no order is found
+  }
+
+  clearOrder() {
+    localStorage.removeItem(this.storageKey); // Clear the order from localStorage
+  }
+
+  private updateLocalStorage() {
+    localStorage.setItem(this.storageKey, JSON.stringify(this.order)); // Update localStorage with the current order
+  }
+
+  decreaseQuantity(product: Product) {
+    const existingProduct = this.order?.productOrders.find(
+      (item) => item.id === product.id
+    );
+
+    if (existingProduct) {
+      existingProduct.quantity -= 1;
+      if (existingProduct.quantity < 1) {
+        this.order.productOrders = this.order.productOrders.filter(
+          (item) => item.id !== product.id
+        );
+      }
+      this.updateLocalStorage(); // Update localStorage after changing quantity
+    }
+
+    console.log('this.order :>> ', this.order);
+  }
+
+  increaseQuantity(product: Product) {
+    const existingProduct = this.order?.productOrders.find(
+      (item) => item.id === product.id
+    );
+
+    if (existingProduct) {
+      existingProduct.quantity += 1;
+    } else {
+      this.order?.productOrders.push({
+        id: product.id,
+        quantity: 1,
+        name: product.name,
+        price: product.price,
+        urlImage: product.urlImage,
+      });
+    }
+    this.updateLocalStorage(); // Update localStorage after adding a product
+  }
+
+  getProductQuantity(productId: string): number {
+    // Retrieve the order from local storage
+    const storedOrder = localStorage.getItem(this.storageKey);
+    if (storedOrder) {
+      this.order = JSON.parse(storedOrder); // Parse the JSON string back to an Order object
+    }
+
+    // Find the product in the order
+    const productOrder = this.order?.productOrders.find(
+      (item) => item.id === productId
+    );
+    return productOrder ? productOrder.quantity : 0; // Return the quantity or 0 if not found
+  }
+
+  saveOrder(): Observable<any> {
+    console.log('Entering saveOrder method...');
+    const orderDataString = localStorage.getItem(this.storageKey);
+
+    console.log('Retrieved orderData from localStorage: ', orderDataString);
+
+    if (orderDataString) {
+      try {
+        const orderData = JSON.parse(orderDataString);
+        console.log('Parsed orderData to JSON:', orderData);
+
+        console.log('Order data found, proceeding to save...');
+        console.log('API URL:', this.apiUrl);
+
+        return this.http.post<any>(this.apiUrl, orderData).pipe(
+          tap(() => {
+            console.log('Order saved successfully.');
+            localStorage.removeItem(this.storageKey);
+            console.log('Order data cleared from localStorage.');
+          }),
+          catchError((error) => {
+            console.error('Error saving order:', error);
+            return of(null);
+          })
+        );
+      } catch (error) {
+        console.error('Error parsing orderData:', error);
+        return of(null);
+      }
+    } else {
+      console.warn('No order data to save.');
+      return of(null);
+    }
+  }
+
+  getTotalItemQuantity(): number {
+    // Retrieve the order from local storage
+    const storedOrder = localStorage.getItem(this.storageKey);
+    if (storedOrder) {
+      this.order = JSON.parse(storedOrder); // Parse the JSON string back to an Order object
+    }
+
+    // Calculate the total quantity of items in the order
+    return (
+      this.order?.productOrders.reduce(
+        (total, item) => total + item.quantity,
+        0
+      ) || 0
+    );
+  }
+  getTotalPrice(): number {
+    // Retrieve the order from local storage
+    const storedOrder = localStorage.getItem(this.storageKey);
+    if (storedOrder) {
+      this.order = JSON.parse(storedOrder); // Parse the JSON string back to an Order object
+    }
+    return (
+      this.order.productOrders.reduce((total: number, item: { price: number; quantity: number; }) => total + (item.price * item.quantity),
+        0
+      ) || 0
+    );
+  }
+}
